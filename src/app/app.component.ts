a3c22279d6e1ea09b31b0675bf02f73b37fe7425
mamjerez <correomam@gmail.com>
2025-01-14 19:16:16 +0100

    refactor app component to fetch data from JSON and update grid options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,33 +15,102 @@ import {
 
 ModuleRegistry.registerModules([AllCommunityModule]);
 
-
-
-
 @Component({
     selector: 'app-root',
     imports: [CommonModule,AgGridModule],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  @ViewChild('agGrid') agGrid!: AgGridAngular;
-  // Row Data: The data to be displayed.
-  rowData = [
-    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-    { make: "Ford", model: "F-Series", price: 33850, electric: false },
-    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-];
+export class AppComponent implements OnInit {
+   @ViewChild('agGrid') agGrid!: AgGridAngular;
+   public rowData1: any[] = [];
+   public gridOptions: GridOptions | undefined;
+   public _columnDefs: ColDef[] | undefined;
+
+   ngOnInit(): void {
+    this.fecthData();
+     }
+
+  async fecthData() { 
+    const data = await import(`../assets/data/csvjson.json`);
+    this.rowData1 = data.default;
+    console.log(this.rowData1);
+      this._setColumnDefs();
+      this._setGridOptions();
+   
+  }
 
 
+  _setColumnDefs() {
+		this._columnDefs = [
+			{
+				headerName: 'Orgánico',
+				field: 'Orgánico',
+				width: 42
+			},
+			{
+				headerName: 'Programa',
+				field: 'Programa',
+				width: 375
+			} as ISetFilterParams,
+			{
+				headerName: 'Económico',
+				field: 'Económico',
+				width: 210
+			},
+			{
+				headerName: 'Descripción',
+				field: 'Descripción',
+				width: 90
+			},
+			{
+				headerName: 'Euros',
+				field: 'Euros',
+				width: 75
+			}
+		];
+	}
 
-// Column Definitions: Defines the columns to be displayed.
-colDefs: ColDef[] = [
-    { field: "make" },
-    { field: "model" },
-    { field: "price" },
-    { field: "electric" }
-];
+
+  _setGridOptions() {
+		this.gridOptions = {
+			defaultColDef: {
+				width: 130,
+				suppressMovable: true,
+				lockPosition: 'left',
+				sortable: true,
+				resizable: true,
+				filter: true,
+				headerComponentParams: {
+					template:
+						'<div class="ag-cell-label-container" role="presentation">' +
+						'  <span ref="eMenu" class="ag-header-icon ag-header-cell-menu-button" ></span>' +
+						'  <div ref="eLabel" class="ag-header-cell-label" role="presentation" >' +
+						'    <span ref="eSortOrder" class="ag-header-icon ag-sort-order"></span>' +
+						'    <span ref="eSortAsc" class="ag-header-icon ag-sort-ascending-icon"></span>' +
+						'    <span ref="eSortDesc" class="ag-header-icon ag-sort-descending-icon"></span>' +
+						'    <span ref="eSortNone" class="ag-header-icon ag-sort-none-icon"></span>' +
+						'    <span ref="eText" class="ag-header-cell-text" role="columnheader" style="white-space: normal;"></span>' +
+						'    <span ref="eFilter" class="ag-header-icon ag-filter-icon"></span>' +
+						'  </div>' +
+						'</div>'
+				}
+			},
+
+			rowData: JSON.parse(JSON.stringify(this.rowData1)),
+			columnDefs: this._columnDefs,
+			groupDisplayType: 'custom',
+			groupIncludeTotalFooter: true,
+			groupIncludeFooter: true,
+			groupHeaderHeight: 25,
+			headerHeight: 24,
+			suppressAggFuncInHeader: true,
+			rowSelection: 'single',
+			// localeText: localeTextESPes,
+			pagination: true,
+			paginationPageSize: 50
+		} as GridOptions;
+	}
 
 defaultColDef: ColDef = {
   flex: 1,
